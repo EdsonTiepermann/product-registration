2e84185d6baa075930cabf8ccd0393a22939ed8f
EdsonTiepermann <tiepermann59@gmail.com>
2024-10-24 09:06:12 -0300

    updates

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ app.use(express.json());
 
 app.use('/products', productsRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+app.listen(process.env.API_PORT, () => {
+    console.log('Server is running on port '+process.env.API_PORT);
+});
