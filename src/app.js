489641f56d449b9cee24f3abff2db27a6baec94d
EdsonTiepermann <tiepermann59@gmail.com>
2024-10-27 21:38:16 -0300

    Updated

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import "dotenv/config";
 import productsRouter from './routes/product-registration-router.js';
+import userRouter from './routes/user-route.js';
 
 const app = express();
 app.use(express.json());
 
+app.use("/user", userRouter);
 app.use('/products', productsRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.API_PORT, () => {
+    console.log('Server is running on port ' + process.env.API_PORT);
   });
